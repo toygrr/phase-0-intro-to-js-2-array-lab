d390b6e949c5b4dbb6622569507964783a94d787
Tyler <toygrrr@gmail.com>
2022-07-20 13:33:57 -0700

    done

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(){
-    cats.push("Ralph");
+function destructivelyAppendCat(name) {
+  return cats.push(name);
 }
 
-function destructivelyPrependCat(){
-    cats.unshift("Bob");
-    }
-
-function destructivelyRemoveLastCat(){
-    cats.pop(-1);
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
 }
 
-function destructivelyRemoveFirstCat(){
-    cats.shift(0);
+function destructivelyRemoveLastCat() {
+  return cats.splice(2, 1);
 }
 
-function appendCat(Broom){
-    return appendCat = [...cats, "Broom"];
+function destructivelyRemoveFirstCat() {
+  return cats.splice(0, 1);
 }
 
-function prependCat(Arnold){
-    return prependCat = ["Arnold", ...cats]
+function appendCat(name) {
+  return [...cats, name];
 }
 
-function removeLastCat(){
-    return cats.slice(0, cats.length - 1)
+function prependCat(name) {
+  return [name, ...cats];
 }
 
-function removeFirstCat(){
-    return cats.slice(1)
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
 }
